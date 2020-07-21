31c39a4f1c03b704e27087e5e0a78559e0758ac2
Tashi Jain <tashjain88@gmail.com>
2020-07-21 14:06:38 -0700

    owl carousel demo

--- js/main.js
+++ js/main.js
@@ -269,35 +269,45 @@
     fixedContentPos: false
   });
 
-  $('.photo_gallery_active').owlCarousel({
-	loop:true,
-	margin:30,
-  items:1,
-  // autoplay:true,
-  navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
-  nav:true,
-  dots:false,
-  // autoplayHoverPause: true,
-  // autoplaySpeed: 800,
-	responsive:{
-		0:{
-			items:1,
-			nav:false
+  $("#owl-demo").owlCarousel({
+ 
+	autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+	items : 4,
+	itemsDesktop : [1199,3],
+	itemsDesktopSmall : [979,3]
+
+});
+
+//   $('.photo_gallery_active').owlCarousel({
+// 	loop:true,
+// 	margin:30,
+//   items:1,
+//   // autoplay:true,
+//   navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+//   nav:true,
+//   dots:false,
+//   // autoplayHoverPause: true,
+//   // autoplaySpeed: 800,
+// 	responsive:{
+// 		0:{
+// 			items:1,
+// 			nav:false
   
-		},
-		767:{
-			items:2,
-			nav:false
-		},
-		992:{
-			items:2,
-			nav:false
-		},
-		1200:{
-			items:2,
-		}
-	}
-  });
+// 		},
+// 		767:{
+// 			items:2,
+// 			nav:false
+// 		},
+// 		992:{
+// 			items:2,
+// 			nav:false
+// 		},
+// 		1200:{
+// 			items:2,
+// 		}
+// 	}
+//   });
   
 
 
