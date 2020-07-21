cba1cf614da3c3c4defd111c09d1dd0ecbf0f5ba
Tashi Jain <tashjain88@gmail.com>
2020-07-21 13:34:11 -0700

    carousel js edit

--- js/main.js
+++ js/main.js
@@ -269,6 +269,37 @@
     fixedContentPos: false
   });
 
+  $('.photo_gallery_active').owlCarousel({
+	loop:true,
+	margin:30,
+  items:1,
+  // autoplay:true,
+  navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+  nav:true,
+  dots:false,
+  // autoplayHoverPause: true,
+  // autoplaySpeed: 800,
+	responsive:{
+		0:{
+			items:1,
+			nav:false
+  
+		},
+		767:{
+			items:2,
+			nav:false
+		},
+		992:{
+			items:2,
+			nav:false
+		},
+		1200:{
+			items:2,
+		}
+	}
+  });
+  
+
 
   var goHere = function() {
 
